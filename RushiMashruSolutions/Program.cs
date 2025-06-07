304fd136750712b5e8d61cc2f74e82a8cf3f9775
Rushi Mashru <rushi.p.mashru@gmail.com>
2025-06-07 23:35:01 +0530

    Add CI/CD for GitHub Pages

--- RushiMashruSolutions/Program.cs
+++ RushiMashruSolutions/Program.cs
@@ -6,10 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddLogging(logging =>
-{
-    logging.SetMinimumLevel(LogLevel.Information);
-    logging.AddConsole();
-});
+// Logging level config only
+builder.Logging.SetMinimumLevel(LogLevel.Information);
 
 await builder.Build().RunAsync();
